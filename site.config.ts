e37eb394c1807013158f366867da592df8984b0b
Oscar <oscartf1@gmail.com>
2024-09-09 22:18:42 +0200

    update icons

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Welcome-to-my-blog-6cf2703d9fe54b6e9fda136b8fbd48fc',
+  rootNotionPageId: 'b7053f2700af4c1bb7bb4af4e3f41954',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
